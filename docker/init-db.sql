81b7191912a40e7b62ee9e02cd2b7783c4c5a897
Brennon Overton <brennonoverton@Brennons-M1.local>
2025-06-27 12:06:08 -0500

    fix db path

--- docker/init-db.sql
+++ docker/init-db.sql
@@ -1,8 +1,2 @@
--- Check if database exists and only create if it doesn't
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'tradetally') THEN
-        PERFORM dblink_exec('dbname=postgres', 'CREATE DATABASE tradetally');
-    END IF;
-END
-$$;
+-- This file ensures the tradetally database exists
+-- PostgreSQL init scripts only run on first container start
