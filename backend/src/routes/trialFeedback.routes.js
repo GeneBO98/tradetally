7f71f40902c9df6a8c43b8b950890af1b33b95e6
GeneBO98 <boverton@tradetally.io>
2026-05-12 08:27:09 -0500

    Revert "feat(email): integrate Sequenzy as email delivery provider"
    This reverts commit 61f7f4c062a392ff3b39da2e482294596caa4117.

--- backend/src/routes/trialFeedback.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const trialFeedbackController = require('../controllers/trialFeedback.controller');
-
-router.get('/', trialFeedbackController.getTrialFeedbackStatus);
-router.post('/', trialFeedbackController.submitTrialFeedback);
-
-module.exports = router;
